9a625d0adf6de31df0fbec09efe5d6aef8ebf009
user <user@debian.localdomain>
2023-01-07 12:15:37 -0600

    user js

---
+++ lyre-tune/m.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+	document.getElementById("headline").innerHTML=`
+<center>
+<strong>
+Lyre Tuning <br /> 
+Application
+</strong>
+</center>
+`;
+});
+
